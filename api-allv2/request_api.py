f5577e102e50e6521229d90d5fbbece698c372e7
Gustavo Olivares <luisgosmx@hotmail.com>
2020-03-25 15:48:27 +0100

    update

--- api-allv2/request_api.py
+++ api-allv2/request_api.py
@@ -11,6 +11,7 @@ from os import listdir
 from os.path import isfile, isdir
 import time
 import termiup_terminal
+from flask import Response
 
 
 REQUEST_API = Blueprint('term_api', __name__)
@@ -27,8 +28,6 @@ def index():
 
 @REQUEST_API.route('/term', methods=['GET'])
 def term():
-    
-
     termino = request.args.get('term')
     idioma = request.args.get('source_language')
     targets1 = request.args.get('target_languages')
@@ -36,15 +35,6 @@ def term():
     wsid = request.args.get('wsid')
     dataRetriever = request.args.get('relation_validation')
     scheme = request.args.get('schema')
-
-
-
-
-
-   
-    
-    
-    
     targets=targets1.split(' ')
     contextlist='' 
     contextFile=None 
@@ -66,5 +56,5 @@ def term():
     jsonlist=termiup_terminal.haceJson(termino, idioma,targets)
     res=termiup_terminal.all(jsonlist, idioma, targets, context, contextFile,  wsid, scheme, dataRetriever,ide)
     print(res)
-    return jsonify(res),200
+    return Response(json.dumps(res),  mimetype='application/json')
       
