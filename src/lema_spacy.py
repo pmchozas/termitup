d063b8da66a71a4f80ffbc52a0376f27952736d6
pmchozas <patriciamartinchozas@gmail.com>
2020-06-18 17:22:29 +0200

    unused files removed

--- src/lema_spacy.py
+++
@@ -1,6 +0,0 @@
-import spacy
-nlp = spacy.load('es_core_news_sm')
-text = 'Soy un texto que pide a gritos que lo procesen. Por eso yo canto, tú cantas, ella canta, nosotros cantamos, cantáis, cantan…'
-doc = nlp(text)
-lemmas = [tok.lemma_.lower() for tok in doc]
-print(lemmas)
